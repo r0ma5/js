c83231a57f9a8c76b505055d3b1e93effe1d095c
Roman Podshivalov <romas@null.net>
2016-03-16 03:05:02 +0000

    romas: test

--- js/isam.js
+++ js/isam.js
@@ -9,7 +9,7 @@ var esd = {
         return this.events.find(function(event){return event.id == id;});
     },
     initiating: function () {
-        return this.events.find(function(event){return event.type == "INITIATING";});
+        return this.events.filter(function(event){return event.type == "INITIATING";});
     },
     outcomes: function () {
         return this.events.filter(function(event){return event.type == "END";});
@@ -18,33 +18,30 @@ var esd = {
         return this.events.filter(function(event){return event.type == "PIVOTAL";});
     },
     calculate: function myself (event, freq, prb){
+        console.log("--------");
         console.log("recursion level:"+gcounts.recursion_level);
-        console.log(event);
         console.log("f:"+freq);
         console.log("p:"+prb);
+        console.log(event);
         if (gcounts.recursion_level++ > gcounts.recursion_depth) return; //safety check to avoid stack issues
         if (event == null || event == undefined){ //find starting point(s)
             console.log("looking for INITIATING event");
-            myself.call(this, this.initiating(), 1, 1);
-        } else if (event.type == "END" && event.outcome == "NEGATIVE") {
-            event._frequency = freq;
-            console.log("Crash:"+event.frequency);
-            return;
-        } else if (event.type == "END" && event.outcome == "POSITIVE") {
-            event._frequency = freq/prb*(1.0-prb);
-            console.log("Continue flight:"+event.frequency);
-            return;
+            this.initiating().forEach(function(e){this.calculate(e,1,1)}, this);
         } else if (event.type == "INITIATING"){
             console.log("Found INITIATING event");
             console.log(event.childIds);
             for (var i = 0; i < event.childIds.length; i++){
-                console.log(event.childIds[i]);
                 myself.call(this, this.fid(event.childIds[i]), event.frequency, 1);
             }
+        } else if (event.type == "END") { //yes branch
+            event._frequency = event.parentRelationType == "YES" ? freq*prb : freq*(1.0-prb);
+            console.log("result:"+event._frequency);
+            return;
         } else if (event.type == "PIVOTAL"){
+            event._frequency = event.parentRelationType == "YES" ? freq*prb : freq*(1.0-prb);
+            console.log("result:"+event._frequency);
             for (var i = 0; i < event.childIds.length; i++){
-                event.frequency = freq*event.probability;
-                myself.call(this, this.fid(event.childIds[i]), event.frequency, event.probability);
+                myself.call(this, this.fid(event.childIds[i]), event._frequency, event.probability);
             }
         }
     }
