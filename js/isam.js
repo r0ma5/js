304c6d3860bbb6973ac146ce8b5c018c986be86c
Roman Podshivalov <romas@null.net>
2016-03-31 02:57:53 +0000

    romas: charts wired

--- js/isam.js
+++ js/isam.js
@@ -11,6 +11,8 @@ Number.prototype.exponent=function(fractionDigits){
     return this.toExponential(fractionDigits).slice(this.toExponential(fractionDigits).indexOf("e")+1);
 };
 
+var esdId;
+
 var esd = {
     events: [],
     fid: function (id){
@@ -41,24 +43,33 @@ var esd = {
             console.log(event.childIds);
             event.childIds.forEach(function(i){this.calculate(this.fid(i), event.frequency,1)},this);
         } else if (event.type == "END") {
-            event._frequency = event.parentRelationType == "YES" ? freq*prb : freq*(1.0-prb);
-            console.log("result:"+event._frequency);
+            event.frequency = event.parentRelationType == "YES" ? freq*prb : freq*(1.0-prb);
+            console.log("result:"+event.frequency);
             return;
         } else if (event.type == "PIVOTAL"){
-            event._frequency = event.parentRelationType == "YES" ? freq*prb : freq*(1.0-prb);
-            console.log("result:"+event._frequency);
-            event.childIds.forEach(function(i){this.calculate(this.fid(i), event._frequency,event.probability)},this);
+            event.frequency = event.parentRelationType == "YES" ? freq*prb : freq*(1.0-prb);
+            console.log("result:"+event.frequency);
+            event.childIds.forEach(function(i){this.calculate(this.fid(i), event.frequency,event.probability)},this);
         }
     }
 };
 
 function displayValue(id, newval){
     console.log("ID:"+id+" VAL:"+newval);
-    console.log(esd.fid(id).probability);
-//    console.log(esd.fid(id).probability.exponent());
-    esd.fid(id).probability=newval;
-    $("#current_"+id).text(esd.fid(id).probability);
+    esd.barriers().forEach(function(e){console.log(e.name+' '+e.probability)});
+    switch(esd.fid(id).type){
+        case "PIVOTAL":
+            esd.fid(id).probability=newval;
+            $("#current_"+id).text(esd.fid(id).probability);
+            break;
+        case "INITIATING":
+            esd.fid(id).frequency=newval;
+            $("#current_"+id).text(esd.fid(id).frequency);
+            break;
+    }    
+    esd.barriers().forEach(function(e){console.log(e.name+' '+e.probability)});
     esd.calculate();
+    drawChart();
 }
 
 function showValue(newValue)
@@ -70,27 +81,90 @@ function showValue(newValue)
 }
 
 function showSlider(b){
+    $("#sliders").append("<p><span>"+b.name+"</span></p>");
     $("#sliders").append("<p>");
     $("#sliders").append("<span>"+b.uniqueId+"</span>");
     $("#sliders").append(b.uniqueID);    
-//    $("#sliders").append("<input type=range id="+b.id+" min=0.000001 max=1 step=.001 value="+Math.round(b.probability.base())+" onchange=displayValue(this.id,this.value) />");
-    $("#sliders").append("<input type=range id="+b.id+" min=0.000001 max=1 step=.000001 value="+b.probability+" onchange=displayValue(this.id,this.value) />");
-    $("#sliders").append("<span id=current_"+b.id+">"+b.probability+"</span>");
+    switch(b.type){
+        case "PIVOTAL":
+            $("#sliders").append("<input type=range id="+b.id+" min=0.00000001 max=1 step=.00000001 value="+b.probability+" onchange=displayValue(this.id,this.value) />");
+            $("#sliders").append("<span id=current_"+b.id+">"+b.probability+"</span>");
+            break;
+        case "INITIATING":
+            $("#sliders").append("<input type=range id="+b.id+" min=0.00000001 max=1 step=.00000001 value="+b.frequency+" onchange=displayValue(this.id,this.value) />");
+            $("#sliders").append("<span id=current_"+b.id+">"+b.frequency+"</span>");
+            break;
+    }
     $("#sliders").append("</p>");
 }
+
+function drawChart() {
+    var i=0;
+// Define the chart to be drawn.
+    console.log("drawChart");
+    esd.outcomes().forEach(function(e){console.log(e.name+' '+e.frequency)});
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', '');
+    esd.initiating().forEach(function(e){
+        data.addColumn('number', e.name);
+        data.addColumn({type: 'string', role: 'tooltip'});
+    });
+    esd.outcomes().forEach(function(e){
+        data.addColumn('number', e.name)
+        data.addColumn({type: 'string', role: 'tooltip'});
+    });
+    data.addRows(2);
+    data.setCell(0, 0, 'Initiating');
+    esd.initiating().forEach(function(e){
+        data.setCell(0, ++i, e.frequency);
+        data.setCell(0, ++i, e.name+" ("+Number(e.frequency).toExponential()+")");
+        
+    });
+    data.setCell(1, 0, 'Outcomes');
+    esd.outcomes().forEach(function(e){
+        data.setCell(1, ++i, e.frequency);
+        data.setCell(1, ++i, e.name+" ("+Number(e.frequency).toExponential()+")");
+    });
+//    esd.outcomes().forEach(function(e){data.addColumn('number', e.frequency)});
+//    data.addColumn('number', 'One');
+//   data.addColumn('number', 'Two');
+//    data.addColumn('number', 'Three');
+//    data.addRows([
+//        ['Initiating', 0.0000001, null, null, null],
+//        ['Outcomes', null, 0.00000078, 0.00000021, 0.00000001],
+//    ]);
+    var options = {
+        title: esdId,
+        width: 600,
+        height: 400,
+        bar: {groupWidth: "95%"},
+        legend: { position: 'right', maxLines: 10 },
+//        legend: { position: "none" },
+        isStacked: true
+    };
+
+      // Instantiate and draw the chart.
+      var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+    }
+
 
 $(document).ready(function() {
     $.ajax({
         url: "https://ape-3.saabsensis.com/isam-webservice/safety/v1/eventSequences/45",
         xhrFields: {withCredentials: true}
     }).then(function(data) {
+      google.charts.load('current', {packages: ['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
         esd.events = data.events;
+        esdId = data.uniqueId;
 //        console.log(esd.fid(440));
        esd.calculate();
        console.log(esd.outcomes());
        console.log(esd.barriers());
+       esd.initiating().forEach(showSlider);
        esd.barriers().forEach(showSlider);
-       $('.greeting-id').append(data.uniqueId);
-       $('.greeting-content').append(data.description);
+       $('.esd-id').append(data.uniqueId);
+       $('.esd-desc').append(data.description);
     });
 });
